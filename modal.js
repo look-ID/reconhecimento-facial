56aa110c5918f913c7651ecf03cccaf3e3d4aeef
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2024-09-16 03:46:29 -0300

    Update

--- modal.js
+++ modal.js
@@ -2,21 +2,22 @@
 function abrir_modal() {
     let modal = document.querySelector('.modal_fundo');
     let janela = document.querySelector('#janela_escura');
-    document.body.classList.add('blur')
+    document.querySelector("main").classList.add("blur")
+    document.querySelector("body").style.overflow="hidden"
 
     modal.style.display='flex'
     janela.style.display='flex'
-    
-
 }
 
 // Função para fechar o modal
 function fechar_modal() {
     let modal = document.querySelector('.modal_fundo');
     let janela = document.querySelector('#janela_escura')
-    document.body.classList.remove('blur')
+    document.querySelector("main").classList.remove("blur")
+    document.querySelector("body").style.overflowY="auto"
     
     modal.style.display='none'
     janela.style.display='none'
-
 }
+
+abrir_modal()
